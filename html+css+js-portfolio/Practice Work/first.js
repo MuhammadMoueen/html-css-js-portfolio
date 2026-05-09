8a601367a8e23b94191a8d76315cb6701665bf5f
Muhammad Moueen <s23bdocs1m01131@iub.edu.pk>
2026-05-09 21:09:14 +0500

    add some practice code

---
+++ html+css+js-portfolio/Practice Work/first.js
@@ -0,0 +1,15 @@
+/*================ Variable & DataType====================*/
+
+const profile = {
+    fullName : "Muhammad Moueen",
+    posts    :     195,
+    followers: "564k",
+    following: 4
+};
+
+profile.posts= profile.posts + 1;
+console.log(profile.posts);
+
+profile.fullName = "Sheikh Moueen";
+
+console.log(profile)
