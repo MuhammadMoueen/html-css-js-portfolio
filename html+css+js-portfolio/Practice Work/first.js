1f7bd165e39ffee73ad96d4e345e9cb76ca7515a
Muhammad Moueen <s23bdocs1m01131@iub.edu.pk>
2026-05-10 11:06:17 +0500

    add some practice code

--- html+css+js-portfolio/Practice Work/first.js
+++ html+css+js-portfolio/Practice Work/first.js
@@ -1,15 +1,79 @@
 /*================ Variable & DataType====================*/
 
-const profile = {
-    fullName : "Muhammad Moueen",
-    posts    :     195,
-    followers: "564k",
-    following: 4
-};
+// const profile = {
+//     fullName : "Muhammad Moueen",
+//     posts    :     195,
+//     followers: "564k",
+//     following: 4
+// };
 
-profile.posts= profile.posts + 1;
-console.log(profile.posts);
+// profile.posts= profile.posts + 1;
+// console.log(profile.posts);
 
-profile.fullName = "Sheikh Moueen";
+// profile.fullName = "Sheikh Moueen";
 
-console.log(profile)
+// console.log(profile)
+
+
+// a = 4;
+// b = "5";
+
+// console.log("a = b:", a == b);
+
+
+//  let x = 10;
+
+// if (x % 2 == 0){
+//     console.log("Number is Even:", x);
+// } 
+// else{
+//     console.log("Number is Old:", x);
+// }
+
+// let x = Number(prompt("Enter The Age Number:"));
+
+// if (x < 18){
+//     console.log("Young Boy");
+// }
+
+// else if (x >= 18 && x <= 25){
+//     console.log("Adult Boy");
+// }
+
+// else{
+//     console.log("Breeder Men");
+
+// }
+
+
+// let x = Number(prompt("Enter The Nmber:"));
+
+// if (x % 5 == 0){
+//     console.log("Number is multiple of 5");
+// }
+// else{
+//     console.log("Number is not multiple of 5");
+
+// }
+
+
+let marks = Number(prompt("Enter The Marks Of Student:"));
+
+if( marks <= 100 && marks >= 80){
+    console.log("Student Grade A");
+}
+else if( marks <= 89 && marks >= 70){
+    console.log("Student Grade B");
+}
+else if( marks <= 69 && marks >= 60){
+    console.log("Student Grade C");
+}
+else if( marks <= 59 && marks >= 50){
+    console.log("Student Grade D");
+}
+else if( marks <= 49){
+    console.log("Student Grade F");
+}
+else{
+    console.log("Invalid Input.")
+}
