180ee093eb0de4c79b1a5cb1b89e4e50ead4cf39
Muhammad Moueen <s23bdocs1m01131@iub.edu.pk>
2026-05-16 11:45:05 +0500

    add some practice code

--- html+css+js-portfolio/Practice Work/script.js
+++ html+css+js-portfolio/Practice Work/script.js
@@ -2,7 +2,14 @@
 // console.dir(h.innerText);
 // h.innerText = h.innerText + " Always Beat Anyone!"
 // console.log(h.innerText);
-let h1 = document.querySelector("h1");
-let span = document.createElement("span");
-span.innerText = " Is Brave!";
-h1.appendChild(span); 
+// let h1 = document.querySelector("h1");
+// let span = document.createElement("span");
+// span.innerText = " Is Brave!";
+// h1.appendChild(span); 
+
+//Create The Butto with styling:
+let btn = document.createElement("button");
+btn.innerText= "Click Me!";
+btn.style.backgroundColor = "red";
+btn.style.color = "white";
+document.body.prepend(btn);
