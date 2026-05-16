7789b1509f8ab0d688a2ee6b320c9b097c1560b0
Muhammad Moueen <s23bdocs1m01131@iub.edu.pk>
2026-05-16 11:23:38 +0500

    add some practice code

--- html+css+js-portfolio/Practice Work/script.js
+++ html+css+js-portfolio/Practice Work/script.js
@@ -1,3 +1,8 @@
-let h = document.querySelector("h1");
-console.dir(h.innerText);
-h.innerText = h.innerText + " Always Best!"
+// let h = document.querySelector("h1");
+// console.dir(h.innerText);
+// h.innerText = h.innerText + " Always Beat Anyone!"
+// console.log(h.innerText);
+let h1 = document.querySelector("h1");
+let span = document.createElement("span");
+span.innerText = " Is Brave!";
+h1.appendChild(span); 
